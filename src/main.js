b8d3dc20562c14dca6a28dc73abfb30608323f58
puzhiyuan <puck.pu@mingdao.com>
2017-09-30 16:03:14 +0800

    test router

--- src/main.js
+++ src/main.js
@@ -2,10 +2,9 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import { domain, fromNow } from './filters';
 import App from './components/App.vue';
-// import App from './components/App.vue';
-import NewsView from './components/NewsView.vue';
-import UserView from './components/UserView.vue';
-import ItemView from './components/ItemView.vue';
+// import NewsView from './components/NewsView.vue';
+// import UserView from './components/UserView.vue';
+// import ItemView from './components/ItemView.vue';
 
 Vue.use(Router);
 
@@ -14,24 +13,47 @@ Vue.filter('domain', domain);
 
 var router = new Router();
 
+// router.map({
+//     '/news/:page': {
+//         component: NewsView
+//     },
+//     '/user/:id': {
+//         component: UserView
+//     },
+//     '/item/:id': {
+//         component: ItemView
+//     },
+// });
+
+// router.beforeEach(function () {
+//     window.scrollTo(0, 0)
+// });
+
+// router.redirect({
+//     '*': '/news/1'
+// });
+
+var Foo = {
+    template: '<div class="foo">' +
+    '<h2>This is Foo</h2>' +
+    '<router-view></router-view>' +
+    '</div>',
+}
+
 router.map({
-    '/news/:page': {
-        component: NewsView
-    },
-    '/user/:id': {
-        component: UserView
-    },
-    '/item/:id': {
-        component: ItemView
-    },
-});
-
-router.beforeEach(function () {
-    window.scrollTo(0, 0)
-});
-
-router.redirect({
-    '*': '/news/1'
-});
+    '/foo': {
+        component: Foo,
+        subRoutes: {
+            '/*username/bar':
+            {
+                component: {
+                    template: '<p>Default sub view for Foo</p>' + 
+                    '<p>router path: {{$route.path}}</p>' + 
+                    '<p>router params: {{$route.params | json}}</p>',
+                }
+            },
+        }
+    }
+})
 
 router.start(App, '#app')
